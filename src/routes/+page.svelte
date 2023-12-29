9871e4a5c4954ea3f20d47dc668883c8feab0a0e
Shinji13 <keskasaymen8@gmail.com>
2023-12-29 15:45:10 +0100

    project setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<div class="main">
+	<img src="/noBackground.png" alt="split's logo" />
+</div>
+
+<style>
+	.main {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		background-color: #ffffff;
+		width: 100vw;
+		height: 100vh;
+		gap: 20px;
+	}
+</style>
